cd17f640a0a70ace4d379ed82178ba5b7f921e78
Niharika Swain <swainniharika27@gmail.com>
2025-08-10 12:32:30 +0530

    added more menus

--- src/data.js
+++ src/data.js
@@ -3,115 +3,477 @@ export const foodData = [
   // Indian Veg
   {
     id: 1,
-    name: 'Mushroom Masala',
-    category: 'Indian Veg',
-    price: '₹120',
-    image: 'https://palatesdesire.com/wp-content/uploads/2022/09/mushroom-masala-recipe@palates-desire.jpg',
+    name: "Mushroom Masala",
+    category: "Indian Veg",
+    price: "₹120",
+    image:
+      "https://palatesdesire.com/wp-content/uploads/2022/09/mushroom-masala-recipe@palates-desire.jpg",
   },
   {
     id: 2,
-    name: 'Mushroom Butter Masala',
-    category: 'Indian Veg',
-    price: '₹130',
-    image: 'https://dhaba-sardardaa.com/wp-content/uploads/2021/12/mushroom-masala.jpg',
+    name: "Mushroom Butter Masala",
+    category: "Indian Veg",
+    price: "₹130",
+    image:
+      "https://dhaba-sardardaa.com/wp-content/uploads/2021/12/mushroom-masala.jpg",
   },
   {
     id: 3,
-    name: 'Panner Masala',
-    category: 'Indian Veg',
-    price: '₹120',
-    image: 'https://www.whiskaffair.com/wp-content/uploads/2020/08/Dhaba-Style-Paneer-Masala-2-3.jpg',
+    name: "Panner Masala",
+    category: "Indian Veg",
+    price: "₹120",
+    image:
+      "https://www.whiskaffair.com/wp-content/uploads/2020/08/Dhaba-Style-Paneer-Masala-2-3.jpg",
   },
   {
     id: 4,
-    name: 'Panner Punjabi',
-    category: 'Indian Veg',
-    price: '₹140',
-    image: 'https://honestcooking.com/wp-content/uploads/2012/09/Paneer-Masala-Recipe.jpg',
+    name: "Panner Punjabi",
+    category: "Indian Veg",
+    price: "₹140",
+    image:
+      "https://honestcooking.com/wp-content/uploads/2012/09/Paneer-Masala-Recipe.jpg",
   },
   {
     id: 5,
-    name: 'Panner Butter Masala',
-    category: 'Indian Veg',
-    price: '₹130',
-    image: 'https://fforflavour.com/wp-content/uploads/2018/08/IMG_5518.jpg',
+    name: "Panner Butter Masala",
+    category: "Indian Veg",
+    price: "₹130",
+    image: "https://fforflavour.com/wp-content/uploads/2018/08/IMG_5518.jpg",
   },
   {
     id: 6,
-    name: 'Kobi Masala',
-    category: 'Indian Veg',
-    price: '₹120',
-    image: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2024/04/gobi-masala-cauliflower-masala-recipe.webp',
+    name: "Kobi Masala",
+    category: "Indian Veg",
+    price: "₹120",
+    image:
+      "https://www.indianhealthyrecipes.com/wp-content/uploads/2024/04/gobi-masala-cauliflower-masala-recipe.webp",
   },
   // Indian Non-veg
   {
     id: 7,
-    name: 'Chicken Masala',
-    category: 'Indian Non Veg',
-    price: '₹120',
-    image: 'https://www.whiskaffair.com/wp-content/uploads/2021/01/Chicken-Masala-2-3-1.jpg',
+    name: "Chicken Masala",
+    category: "Indian Non Veg",
+    price: "₹120",
+    image:
+      "https://www.whiskaffair.com/wp-content/uploads/2021/01/Chicken-Masala-2-3-1.jpg",
   },
   {
     id: 8,
-    name: 'Chicken Butter Masala',
-    category: 'Indian Non Veg',
-    price: '₹140',
-    image: 'https://images.immediate.co.uk/production/volatile/sites/30/2021/02/butter-chicken-ac2ff98.jpg?quality=90&resize=440,400',
+    name: "Chicken Butter Masala",
+    category: "Indian Non Veg",
+    price: "₹140",
+    image:
+      "https://images.immediate.co.uk/production/volatile/sites/30/2021/02/butter-chicken-ac2ff98.jpg?quality=90&resize=440,400",
   },
   {
     id: 9,
-    name: 'Chicken Punjabi',
-    category: 'Indian Non Veg',
-    price: '₹140',
-    image: 'https://onestophalal.com/cdn/shop/articles/punjabi_chicken_curry-1699075119880_1200x.jpg?v=1699075165',
+    name: "Chicken Punjabi",
+    category: "Indian Non Veg",
+    price: "₹140",
+    image:
+      "https://onestophalal.com/cdn/shop/articles/punjabi_chicken_curry-1699075119880_1200x.jpg?v=1699075165",
   },
   {
     id: 10,
-    name: 'Chicken Kadai',
-    category: 'Indian Non Veg',
-    price: '₹130',
-    image: 'https://static.toiimg.com/thumb/53523514.cms?imgsize=102389&width=800&height=800',
+    name: "Chicken Kadai",
+    category: "Indian Non Veg",
+    price: "₹130",
+    image:
+      "https://static.toiimg.com/thumb/53523514.cms?imgsize=102389&width=800&height=800",
   },
   {
     id: 11,
-    name: 'Chicken Hydrabadi',
-    category: 'Indian Non Veg',
-    price: '₹140',
-    image: 'https://theyummydelights.com/wp-content/uploads/2019/07/hyderabadi-chicken-masala-9.jpg',
+    name: "Chicken Hydrabadi",
+    category: "Indian Non Veg",
+    price: "₹140",
+    image:
+      "https://theyummydelights.com/wp-content/uploads/2019/07/hyderabadi-chicken-masala-9.jpg",
   },
   {
     id: 12,
-    name: 'Garlic Chicken',
-    category: 'Indian Non Veg',
-    price: '₹130',
-    image: 'https://www.thespruceeats.com/thmb/tGcR66jMgCb0KUbl-zus8GjKeN0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/instant-pot-honey-garlic-chicken-recipe-5204292-hero-01-108a039fdc644a1ca26df9e0c170dac4.jpg',
+    name: "Garlic Chicken",
+    category: "Indian Non Veg",
+    price: "₹130",
+    image:
+      "https://www.thespruceeats.com/thmb/tGcR66jMgCb0KUbl-zus8GjKeN0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/instant-pot-honey-garlic-chicken-recipe-5204292-hero-01-108a039fdc644a1ca26df9e0c170dac4.jpg",
   },
   {
     id: 13,
-    name: 'Ginger Chicken',
-    category: 'Indian Non Veg',
-    price: '₹130',
-    image: 'https://christieathome.com/wp-content/uploads/2023/06/Ginger-Chicken-16-1.jpg',
+    name: "Ginger Chicken",
+    category: "Indian Non Veg",
+    price: "₹130",
+    image:
+      "https://christieathome.com/wp-content/uploads/2023/06/Ginger-Chicken-16-1.jpg",
   },
   {
     id: 14,
-    name: 'Chicken Mughlai',
-    category: 'Indian Non Veg',
-    price: '₹130',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXt6hf6t_sE2H3Yo1SW9hNskxJ7Cl2qj3N5A&s',
+    name: "Chicken Mughlai",
+    category: "Indian Non Veg",
+    price: "₹130",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXt6hf6t_sE2H3Yo1SW9hNskxJ7Cl2qj3N5A&s",
   },
   {
     id: 15,
-    name: 'Chicken Dopiaza',
-    category: 'Indian Non Veg',
-    price: '₹150',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1komEoBwo4fuWwWHCwhGG_fPn-p1Yjv_U5Q&s',
+    name: "Chicken Dopiaza",
+    category: "Indian Non Veg",
+    price: "₹150",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1komEoBwo4fuWwWHCwhGG_fPn-p1Yjv_U5Q&s",
   },
   {
     id: 16,
-    name: 'Rice Chicken',
-    category: 'Indian Non Veg',
-    price: '₹130',
-    image: 'https://static.vecteezy.com/system/resources/thumbnails/023/830/963/small/mixed-rice-with-chicken-slice-ai-generate-photo.jpg',
+    name: "Rice Chicken",
+    category: "Indian Non Veg",
+    price: "₹130",
+    image:
+      "https://static.vecteezy.com/system/resources/thumbnails/023/830/963/small/mixed-rice-with-chicken-slice-ai-generate-photo.jpg",
+  },
+  // Fried
+  {
+    id: 17,
+    name: "Veg Fried Rice",
+    category: "Fried",
+    price: "₹40",
+    image:
+      "https://slurrp.club/wp-content/uploads/2022/04/DSC_0286-2-2-750x481.jpg",
+  },
+  {
+    id: 18,
+    name: "Paneer Fried Rice",
+    category: "Fried",
+    price: "₹70",
+    image:
+      "https://www.kannammacooks.com/wp-content/uploads/paneer-fried-rice-recipe-1-5.jpg",
+  },
+  {
+    id: 19,
+    name: "Mushroom Fried Rice",
+    category: "Fried",
+    price: "₹70",
+    image:
+      "https://palatesdesire.com/wp-content/uploads/2021/11/mushroom-fried-rice@palates-desire-1.jpg",
+  },
+  {
+    id: 20,
+    name: "Mix Veg Fried Rice",
+    category: "Fried",
+    price: "₹80",
+    image:
+      "https://s.lightorangebean.com/media/20240914154201/Mixed-Veg-Rice-Delight_-done.png",
+  },
+  {
+    id: 21,
+    name: "Tomato Fried Rice",
+    category: "Fried",
+    price: "₹70",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZrtYSYPsnFCCAcNGQ3YsTtUv-benXxgRE8A&s",
+  },
+  {
+    id: 22,
+    name: "Egg Schezwan Fried Rice",
+    category: "Fried",
+    price: "₹80",
+    image: "https://cdn.uengage.io/uploads/7057/image-3244-1666791057.jpg",
+  },
+  {
+    id: 23,
+    name: "Chicken Fried Rice",
+    category: "Fried",
+    price: "₹60",
+    image:
+      "https://hungrycookskitchen.com/wp-content/uploads/2025/01/Chinese-Chicken-Fried-Rice-24.jpg",
+  },
+  {
+    id: 24,
+    name: "Egg Chicken Fried Rice",
+    category: "Fried",
+    price: "₹70",
+    image: "https://static.toiimg.com/thumb/75581339.cms?width=1200&height=900",
+  },
+  {
+    id: 25,
+    name: "Egg Fried Rice",
+    category: "Fried",
+    price: "₹50",
+    image:
+      "https://christieathome.com/wp-content/uploads/2021/07/Egg-Fried-Rice-5-500x500.jpg",
+  },
+  // Chinese Veg
+  {
+    id: 26,
+    name: "Mushroom 65",
+    category: "Chinese Veg",
+    price: "₹120",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-3BpGpk2wkGeg79ptpVveMe0opqjPNCuzrQ&s",
+  },
+  {
+    id: 27,
+    name: "Mushroom Pepper Dry",
+    category: "Chinese Veg",
+    price: "₹110",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRAy1A2ranOzWCwSi5FNw3TdyWjNLFYsC7aA&s",
+  },
+  {
+    id: 28,
+    name: "Mushroom Manchurian",
+    category: "Chinese Veg",
+    price: "₹110",
+    image:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiXBjTTzx8lTHyQj6BkrygZUP_iQSB1ps9pesiV3DrHHqdh4uBg9P_sbPFXF6hFuSFR-X5wy5d6FQXF92K4o-CKd5h_B0bxU1QZquqBhAnhEpOn4XgRRqJvuvb2a-ke8uTWvT16LE8qMHDA/s1600/Mushroom+Manchurian+2.jpg",
+  },
+  {
+    id: 29,
+    name: "Mushroom Chilli",
+    category: "Chinese Veg",
+    price: "₹120",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiq_XEmrGNKGrwIMVCbYhByBMVMmuCTY2SgQ&s",
+  },
+  {
+    id: 30,
+    name: "Paneer 65",
+    category: "Chinese Veg",
+    price: "₹110",
+    image:
+      "https://cdn1.foodviva.com/static-content/food-images/snacks-recipes/paneer-65-recipe/paneer-65-recipe.jpg",
+  },
+  {
+    id: 31,
+    name: "Paneer Pepper Dry",
+    category: "Chinese Veg",
+    price: "₹100",
+    image:
+      "https://www.cookingandme.com/wp-content/uploads/2012/09/8022229921_622e044907_z.jpg",
+  },
+  {
+    id: 32,
+    name: "Paneer Manchurian",
+    category: "Chinese Veg",
+    price: "₹100",
+    image:
+      "https://www.awesomecuisine.com/wp-content/uploads/2008/02/Paneer_Manchurian-500x500.jpg",
+  },
+  {
+    id: 33,
+    name: "Paneer Chilli",
+    category: "Chinese Veg",
+    price: "₹120",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrS73MLaoVpSA4zj-DGllu00amg_H7oWYsw&s",
+  },
+  {
+    id: 34,
+    name: "Paneer Do Pyaza",
+    category: "Chinese Veg",
+    price: "₹140",
+    image:
+      "https://static.toiimg.com/thumb/59628941.cms?imgsize=364393&width=800&height=800",
+  },
+  {
+    id: 35,
+    name: "Veg Manchurian",
+    category: "Chinese Veg",
+    price: "₹60",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoznLy2QSLmkY4XvKkeEjHS1M6O9uKDy5tsQ&s",
+  },
+  // Chinese Non-Veg
+  {
+    id: 36,
+    name: "Chicken 65",
+    category: "Chinese Non-Veg",
+    price: "₹120",
+    image: "https://i.ytimg.com/vi/afaP90FH-MA/maxresdefault.jpg",
+  },
+  {
+    id: 37,
+    name: "Chicken Pepper Dry",
+    category: "Chinese Non-Veg",
+    price: "₹110",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTm7xEzqYlELiPyiv-VvBu8JeqPV54gugqW4g&s",
+  },
+  {
+    id: 38,
+    name: "Chicken Manchurian",
+    category: "Chinese Non-Veg",
+    price: "₹120",
+    image: "https://sinfullyspicy.com/wp-content/uploads/2020/12/3-2.jpg",
+  },
+  {
+    id: 39,
+    name: "Chicken Chilli",
+    category: "Chinese Non-Veg",
+    price: "₹100",
+    image:
+      "https://images.slurrp.com/prod/recipe_images/transcribe/side%20dish/Chilli_Chicken.webp?impolicy=slurrp-20210601&width=1200&height=900&q=75",
+  },
+  {
+    id: 40,
+    name: "Lemon Chicken",
+    category: "Chinese Non-Veg",
+    price: "₹130",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkZaVncK_3HgPiKfRXA6M3vf2sHdQ59TRKng&s",
+  },
+  {
+    id: 41,
+    name: "Dry Chicken",
+    category: "Chinese Non-Veg",
+    price: "₹140",
+    image:
+      "https://www.kannammacooks.com/wp-content/uploads/chicken-roast-recipe-1-4.jpg",
+  },
+  // Noodles
+  {
+    id: 42,
+    name: "Veg Chowmein",
+    category: "Noodles",
+    price: "₹40",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDexLL8JgTP8-yFKImrj2JDx-yxykHJWwxJQ&s",
+  },
+  {
+    id: 43,
+    name: "Paneer Chowmein",
+    category: "Noodles",
+    price: "₹60",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzntn8EP7u6YEQXRXROqeUIhpae5-q5YohGw&s",
+  },
+  {
+    id: 44,
+    name: "Mushroom Chowmein",
+    category: "Noodles",
+    price: "₹70",
+    image:
+      "https://images.slurrp.com/prod/recipe_images/transcribe/lunch/Mushroom-chow-mein.webp?impolicy=slurrp-20210601&width=1200&height=675",
+  },
+  {
+    id: 45,
+    name: "Mix Veg Chowmein",
+    category: "Noodles",
+    price: "₹80",
+    image:
+      "https://s.lightorangebean.com/media/20240914164843/chow-mein-fun-done.png",
+  },
+  {
+    id: 46,
+    name: "Veg Schezwan Chowmein",
+    category: "Noodles",
+    price: "₹50",
+    image:
+      "https://spoonsofflavor.com/wp-content/uploads/2021/04/Vegetable-Chow-Mein-Recipe-3.jpg",
+  },
+  {
+    id: 47,
+    name: "Egg Chowmein",
+    category: "Noodles",
+    price: "₹50",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfUxoxHPWjePV8D1nT089UT8yWTDYNw3ro5w&s",
+  },
+  {
+    id: 48,
+    name: "Chicken Chowmein",
+    category: "Noodles",
+    price: "₹50",
+    image:
+      "https://i.pinimg.com/736x/a4/ac/2c/a4ac2c0e7e32e0538f49c7628059fafa.jpg",
+  },
+  {
+    id: 49,
+    name: "Egg Chicken Chowmein",
+    category: "Noodles",
+    price: "₹70",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqnILIGGc9ZQJ4Iisl8U4Skga-8Jdl3x5C-Q&s",
+  },
+  {
+    id: 50,
+    name: "Non-Veg Schezwan Chowmein",
+    category: "Noodles",
+    price: "₹80",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRH5r-zc3NqMQbdNpcWZZn3a7_Q9rP8vugyg&s",
+  },
+
+  // Roll's And Pakodas
+  {
+    id: 51,
+    name: "Veg Roll",
+    category: "Rolls And Pakoda",
+    price: "₹40",
+    image:
+      "https://c.ndtvimg.com/2024-02/bbvgfbmg_kathi-rolls_625x300_12_February_24.jpg",
+  },
+  {
+    id: 52,
+    name: "Paneer Roll",
+    category: "Rolls And Pakoda",
+    price: "₹60",
+    image:
+      "https://spicecravings.com/wp-content/uploads/2020/12/Paneer-kathi-Roll-Featured-1.jpg",
+  },
+  {
+    id: 53,
+    name: "Mushroom Roll",
+    category: "Rolls And Pakoda",
+    price: "₹60",
+    image:
+      "https://i.ytimg.com/vi/pmiPqdICN20/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AG2CIACuAiKAgwIABABGH8gUigTMA8=&rs=AOn4CLAHV8YwpqBkdJ6OR_vUk0AU8aqtqQ",
+  },
+  {
+    id: 54,
+    name: "Egg Roll(Single)",
+    category: "Rolls And Pakoda",
+    price: "₹40",
+    image: "https://fuddins.com/wp-content/uploads/2023/03/60f2ea67b471327a1d82959b_chicken-roll_1500-x-1200.jpg",
+  },
+  {
+    id: 55,
+    name: "Egg Roll(Double)",
+    category: "Rolls And Pakoda",
+    price: "₹50",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWq2E8MSBW03UqDeNdliBejBKm2kuM2Mdveg&s",
+  },
+  {
+    id: 56,
+    name: "Chicken Roll",
+    category: "Rolls And Pakoda",
+    price: "₹40",
+    image: "https://madscookhouse.com/wp-content/uploads/2021/02/Chicken-Kathi-Roll-500x500.jpg",
+  },
+  {
+    id: 57,
+    name: "Egg Chicken Roll(Single)",
+    category: "Rolls And Pakoda",
+    price: "₹50",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRymRIYBxJX7dk_CUFiwEJ9Fs21CqxYh82lHA&s",
+  },
+  {
+    id: 58,
+    name: "Egg Chicken Roll(Double)",
+    category: "Rolls And Pakoda",
+    price: "₹60",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXnmLSEsaNFJk1joGBu16Oby9iSDyJ5cR8bA&s",
+  },
+  {
+    id: 59,
+    name: "Chicken Pakoda(Half)",
+    category: "Rolls And Pakoda",
+    price: "₹60",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZAQb4Z4tGzJZclfF6_s21SKyGbbhWPV9JWQ&s",
+  },
+  {
+    id: 60,
+    name: "Chicken Pakoda(Full)",
+    category: "Rolls And Pakoda",
+    price: "₹120",
+    image: "https://i.ytimg.com/vi/xlszcRPKDqk/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLABDSmLpkrMPZKWZQMV7T84exmt4g",
   },
 ];
